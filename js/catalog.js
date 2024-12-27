eb125d58c01dc423b23e0a40622f3e2b4891c5f8
Vazovsk1y <anolrey.b@gmail.com>
2024-12-27 08:39:26 +0300

    -initial commit.

---
+++ js/catalog.js
@@ -0,0 +1,121 @@
+﻿const allGenresOption = "Все жанры";
+const allYearsOption = "Все года";
+const allCountriesOption = "Все страны";
+
+function loadData(year, genre, country){
+    const params = new URLSearchParams(window.location.search);
+    const type = params.get('type');
+    if (!type) {
+        return;
+    }
+
+    let values = data.filter(e => e.subType === type);
+    if (year && year !== allYearsOption){
+        // Compare string with year.
+        values = values.filter(e => e.year == year);
+    }
+
+    if (genre && genre !== allGenresOption){
+        values = values.filter(e => e.genre.includes(genre));
+    }
+
+    if (country && country !== allCountriesOption){
+        values = values.filter(e => e.country.includes(country));
+    }
+
+    const cardsContainer = document.getElementById("cards-list");
+    cardsContainer.innerHTML = "";
+
+    values.forEach(e => {
+        const card = document.createElement("li");
+        card.classList.add("flex-container");
+        card.classList.add("card");
+
+        card.innerHTML = `
+        <img alt="cover" class="card-cover" src="${e.coverUrl + "72x108"}">
+        <div class="flex-container card-info">
+            <div class="flex-container card-info">
+                <h3>${e.ruTitle}</h3>
+                <p>${e.enTitle}, ${e.year}, ${e.duration}</p>
+                <span>${e.genre}</span>
+                <span>${e.country} - Режиссёр: ${e.director}</span>
+                <span>В ролях: ${e.starring.join(", ")}</span>
+            </div>
+        </div>
+        <div class="flex-container card-right">
+            <p style="color: goldenrod; font-size: 22px">${e.rating}</p>
+        </div>
+        `;
+
+        cardsContainer.appendChild(card);
+    });
+}
+
+function initializeSelects(){
+    const years = document.getElementById("years-select");
+    const genres = document.getElementById("genres-select");
+    const countries = document.getElementById("countries-select");
+    const currentYear = new Date(Date.now()).getFullYear();
+
+    let firstOption = document.createElement("option");
+    firstOption.value = allYearsOption;
+    firstOption.textContent = allYearsOption;
+    firstOption.selected = true;
+
+    years.appendChild(firstOption);
+
+    const lower = 1970;
+    for (let i = currentYear; i > lower; i--) {
+        const option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        years.appendChild(option);
+    }
+
+    firstOption = document.createElement("option");
+    firstOption.value = allGenresOption;
+    firstOption.textContent = allGenresOption;
+    firstOption.selected = true;
+
+    genres.innerHTML = `
+    ${firstOption.outerHTML}
+    ${genres.innerHTML}   
+    `;
+
+    firstOption = document.createElement("option");
+    firstOption.value = allCountriesOption;
+    firstOption.textContent = allCountriesOption;
+    firstOption.selected = true;
+
+    countries.innerHTML = `
+    ${firstOption.outerHTML}
+    ${countries.innerHTML}
+    `;
+
+    years.addEventListener("change", (e) => {
+        const selectedYear = e.target.value;
+        const selectedGenre = genres.value;
+        const selectedCountry = countries.value;
+
+        loadData(selectedYear, selectedGenre, selectedCountry);
+    });
+
+    genres.addEventListener("change", (e) => {
+        const selectedYear = years.value;
+        const selectedGenre = e.target.value;
+        const selectedCountry = countries.value;
+
+        loadData(selectedYear, selectedGenre, selectedCountry);
+    });
+
+    countries.addEventListener("change", (e) => {
+        const selectedYear = years.value;
+        const selectedGenre = genres.value;
+        const selectedCountry = e.target.value;
+
+        loadData(selectedYear, selectedGenre, selectedCountry);
+    });
+}
+
+loadData(allYearsOption, allGenresOption, allCountriesOption);
+initializeSelects();
---
+++ js/index.js
@@ -0,0 +1,70 @@
+﻿function loadGroups() {
+    const rootElement = document.getElementById("root");
+    const groups = Object.groupBy(data, ({ subType }) => subType);
+
+    Object.entries(groups).forEach(([key, value]) => {
+        const groupCard = document.createElement("div");
+        groupCard.classList.add("group");
+        groupCard.classList.add("flex-container");
+
+        const groupTitle = document.createElement("button");
+        groupTitle.classList.add("group-title");
+        groupTitle.textContent = key;
+
+        const groupSlider = document.createElement("div");
+
+        groupSlider.classList.add("group-slider");
+        groupSlider.classList.add("flex-container");
+
+        const groupItems = document.createElement("div");
+        groupItems.classList.add("group-items-container");
+        groupItems.classList.add("flex-container");
+        groupItems.classList.add("swiper-wrapper");
+
+        value.forEach((item) => {
+            item.randomId = crypto.randomUUID();
+        });
+
+        value.sort((a, b) => a.randomId.localeCompare(b.randomId));
+
+        value.forEach((item) => {
+            const groupItem = document.createElement("div");
+            groupItem.classList.add("group-item");
+            groupItem.classList.add("swiper-slide");
+            groupItem.classList.add("flex-container");
+
+            groupItem.innerHTML = `
+    <div class="cover-container">
+        <img alt="cover" src="${item.coverUrl + "300x450"}" >
+        <div class="cover-info">
+            <p style="font-size: 20px; font-weight: 600">${item.rating}</p>
+            <p style="font-size: 16px">${item.year}, ${
+                item.genre.split(",")[0]
+            }</p>
+            <p style="font-size: 16px">${item.country}</p>
+            <p style="font-size: 16px">${item.duration}</p>
+        </div>
+    </div>
+    <div style="margin-top: 10px">
+        <button class="item-title">${item.ruTitle}</button>
+        <button style="display: block; color: red; cursor: pointer; font-size: 14px; margin-top: 5px; background: transparent">Подписка</button>
+    </div>`;
+
+            groupItems.appendChild(groupItem);
+        });
+
+        groupSlider.innerHTML = `
+            <div class="items-wrapper mySwiper swiper">
+                ${groupItems.outerHTML}
+                <div class="swiper-button-next nextBtn"></div>
+                  <div class="swiper-button-prev prevBtn"></div>
+            </div>
+        `;
+
+        groupCard.appendChild(groupTitle);
+        groupCard.appendChild(groupSlider);
+        rootElement.appendChild(groupCard);
+    });
+}
+
+loadGroups();
